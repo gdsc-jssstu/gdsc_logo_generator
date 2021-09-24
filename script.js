0b2e90181709434dbe03263dcefd92c25785d767
ss467 <72782784+ss467@users.noreply.github.com>
2021-09-24 16:53:23 +0530

    Base project with responsive front-end.
    Base project and responsive front-end for the logo-generator with a PNG image downloadable.

---
+++ script.js
@@ -0,0 +1,44 @@
+ //Javascript Code for Logo generator website
+
+
+ //Script for responsive navbar
+ const toggleButton = document.getElementsByClassName('toggle')[0]
+ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+ toggleButton.addEventListener('click',()=>{
+   navbarLinks.classList.toggle('active')
+ })
+
+
+
+
+//Script for Logo Creation and Download
+  document.getElementById("college-name").addEventListener('input', (e) => {
+      let college_name_thingy = document.getElementsByClassName("college-name");
+      college_name_thingy.forEach(thingy => thingy.innerHTML = e.target.value);
+      update();
+  });
+  let update = (first = false) => {
+      let svgviews = document.getElementsByClassName("svg-view");
+      svgviews.forEach(view => {
+          const canvas = view.getElementsByTagName("canvas")[0];
+          const svg = view.getElementsByTagName("svg")[0];
+          if (first) {
+              const button = view.getElementsByTagName("button")[0];
+              button.addEventListener('click', () => {
+                  var image = canvas.toDataURL("image/png");
+                  let tmp = document.createElement('a');
+                  tmp.download = "dsclogo.png";
+                  tmp.href = image;
+
+                  document.body.appendChild(tmp);
+                  tmp.click();
+                  document.body.removeChild(tmp);
+              });
+          }
+          const ctx = canvas.getContext('2d');
+          v = canvg.Canvg.fromString(ctx, svg.outerHTML);
+          v.start();
+      });
+  };
+  update(true);
